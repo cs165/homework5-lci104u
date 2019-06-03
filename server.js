4d8c902f52d66d98f2ce25d8bb06e5e759ef4cd1
lci104u <lci104u@cs.ccu.edu.tw>
2019-06-04 02:57:46 +0800

    complete
    complete

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const key = require('./privateSettings.json');
 
 // TODO(you): Change the value of this string to the spreadsheet id for your
 // GSA spreadsheet. See HW5 spec for more information.
-const SPREADSHEET_ID = '__YOUR__SPREADSHEET__ID__HERE__';
+const SPREADSHEET_ID = '1rpj_D8j3Mh9GdO8Ri63Dx3yeILup6xgrb0NRNkYIBSg';
 
 const app = express();
 const jsonParser = bodyParser.json();
@@ -20,8 +20,16 @@ async function onGet(req, res) {
   console.log(rows);
 
   // TODO(you): Finish onGet.
+  const title = rows[0];
+  const newObj = rows.slice(1).map(val => {
+	  const empObj={};
+	  title.forEach( (val2 , ind) => {
+		  empObj[val2] = val[ind];
+	  });
+	  return empObj;
+  });
 
-  res.json( { status: 'unimplemented'} );
+  res.json( newObj );
 }
 app.get('/api', onGet);
 
@@ -29,8 +37,17 @@ async function onPost(req, res) {
   const messageBody = req.body;
 
   // TODO(you): Implement onPost.
+  const result = await sheet.getRows();
+  const rows = result.rows;
+  const title = rows[0];
+  const oldAry = [];
+  Object.keys(messageBody).forEach( val3 => {
+	const inInd = title.findIndex( val4 => val4.toLowerCase() === val3.toLowerCase());
+	oldAry[inInd] = messageBody[val3];
+  });
+  await sheet.appendRow( oldAry );
 
-  res.json( { status: 'unimplemented'} );
+  res.json( { response: 'success'}  );
 }
 app.post('/api', jsonParser, onPost);
 
@@ -40,8 +57,21 @@ async function onPatch(req, res) {
   const messageBody = req.body;
 
   // TODO(you): Implement onPatch.
-
-  res.json( { status: 'unimplemented'} );
+  const result = await sheet.getRows();
+  const rows = result.rows;
+  const thisInd = rows[0].findIndex( val => val.toLowerCase() === column.toLowerCase() );
+  if (thisInd > -1) {
+	  const rightInd = rows.slice(1).findIndex( val2 => val2[thisInd].toLowerCase() === value.toLowerCase());
+	  if ( rightInd > -1 ) {
+		  const oldAry = rows[rightInd+1];
+		  Object.keys(messageBody).forEach( val3 => {
+			  const inInd = rows[0].findIndex( val4 => val4.toLowerCase() === val3.toLowerCase());
+			  oldAry[inInd] = messageBody[val3];
+		  });
+		  await sheet.setRow( rightInd+1 , oldAry );
+	  }
+  } 
+  res.json( { response: 'success'} );
 }
 app.patch('/api/:column/:value', jsonParser, onPatch);
 
@@ -50,8 +80,14 @@ async function onDelete(req, res) {
   const value  = req.params.value;
 
   // TODO(you): Implement onDelete.
-
-  res.json( { status: 'unimplemented'} );
+  const result = await sheet.getRows();
+  const rows = result.rows;
+  const thisInd = rows[0].findIndex( val => val.toLowerCase() === column.toLowerCase() );
+  if (thisInd > -1) {
+	  const rightInd = rows.slice(1).findIndex( val2 => val2[thisInd].toLowerCase() === value.toLowerCase());
+	  if ( rightInd > -1 ) await sheet.deleteRow( rightInd+1 );
+  } 
+  res.json( { response: 'success'} );
 }
 app.delete('/api/:column/:value',  onDelete);
 
